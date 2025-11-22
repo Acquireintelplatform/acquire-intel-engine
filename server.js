e3145546e6150de7438960f1937bd75145b53a08
Acquireintelplatform <acquireintel@gmail.com>
2025-11-22 15:57:25 +0000

    Update server.js

--- server.js
+++ server.js
@@ -3,23 +3,28 @@ import cors from "cors";
 import dotenv from "dotenv";
 dotenv.config();
 
-// ==============================
-// IMPORT API ROUTER
-// ==============================
+// ================================
+// IMPORT API ROUTES
+// ================================
 import apiRouter from "./src/api/index.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-// ==============================
+// Root test route
+app.get("/", (req, res) => {
+  res.send("Acquire Backend Running OK");
+});
+
+// ================================
 // USE API ROUTER
-// ==============================
+// ================================
 app.use("/api", apiRouter);
 
-// ==============================
+// ================================
 // START SERVER
-// ==============================
+// ================================
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
