076109500ce02dd35816b6cbb4a4fb931230bf33
Acquireintelplatform <acquireintel@gmail.com>
2025-11-22 13:31:47 +0000

    Create sicCodes.js

---
+++ src/distress/sicCodes.js
@@ -0,0 +1,31 @@
+export const FBS_SIC_CODES = [
+  // Restaurants, Takeaways, Cafés, Bars, Pubs
+  "56101", // Licensed restaurants
+  "56102", // Unlicensed restaurants and cafes
+  "56103", // Take-away food shops and mobile food stands
+  "56301", // Bars & pubs
+  "56302", // Public houses and bars
+
+  // Catering
+  "56210", // Event catering
+  "56290", // Other food service activities
+
+  // Hospitality + Accommodation
+  "55100", // Hotels
+  "55201", // Holiday centres
+  "55209", // Other holiday accommodation
+
+  // Leisure + Fitness
+  "93110", // Sports facilities
+  "93120", // Activities of sport clubs
+  "93130", // Fitness facilities / gyms
+  "93290", // Other amusement & recreation
+
+  // Nurseries + Childcare
+  "85100", // Pre-primary education (nursery schools)
+  "88910", // Child day-care activities
+
+  // Nightclubs
+  "56301", // Nightclubs (also bars/pubs)
+  "93290", // Late-night entertainment venues
+];
