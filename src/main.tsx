1928f5ee6650422b36f9fbb718b62e86f12f9ccd
Acquireintelplatform <acquireintel@gmail.com>
2025-12-16 16:07:20 +0000

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,16 @@
 // src/main.tsx
 import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-
+import { createRoot } from "react-dom/client";
 import App from "./App";
-
-// IMPORTANT: these two lines pull in your styles
-import "./global.css";
 import "./index.css";
 
-console.log("MAIN TSX IS RUNNING");
+const rootEl = document.getElementById("root");
+if (!rootEl) {
+  throw new Error("Missing <div id=\"root\"></div> in index.html");
+}
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+createRoot(rootEl).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
