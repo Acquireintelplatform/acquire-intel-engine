9905f38cdaec5cfcc872a18da31138b52160806e
Acquireintelplatform <acquireintel@gmail.com>
2025-12-18 15:39:30 +0000

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-// src/main.tsx
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
@@ -6,7 +5,7 @@ import "./index.css";
 
 const rootEl = document.getElementById("root");
 if (!rootEl) {
-  throw new Error("Missing <div id=\"root\"></div> in index.html");
+  throw new Error('Missing <div id="root"></div> in index.html');
 }
 
 createRoot(rootEl).render(
